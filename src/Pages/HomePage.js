7147e7de8827c93def1d05e267d71cf3de535616
Kyle Stewart-Frantz <kylesf@users.noreply.github.com>
2023-08-08 21:22:02 -0700

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,36 +1,60 @@
-import React from 'react'
-import {faLinkedin, faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import avatar from '../img/avatar.png';
 
 function HomePage() {
-    return (
-        <div className="HomePage">
-            <header className="hero">
-                <div className="profile">
-                        <img src={avatar} alt=""/>
-                </div>
-                <h1 className="hero-text">
-                    Hi, I'm
-                    <span> Kyle </span>
-                </h1>
-                <p className="h-sub-text">
-                A software engineer focused on Full Stack Development, Data Pipelines and Technical Architecture in People Analytics at <a href='https://www.roche.com'>Roche</a>. <br />SMB Owner of  <a href='https://www.theclearicecompany.com'>The Clear Ice Company</a>
-                </p>
-                <div className="icons">
-                    <a href='https://www.linkedin.com/in/kylestewartfrantz/' className="icon-holder" >
-                        <FontAwesomeIcon icon={faLinkedin} className="icon fb"/>    
-                    </a>
-                    // <a href='https://www.github.com/kylesf/' className="icon-holder">
-                    //     <FontAwesomeIcon icon={faGithub} className="icon gh"/>    
-                    // </a>
-                    <a href='https://instagram.com/theclearicecompany/' className="icon-holder">
-                        <FontAwesomeIcon icon={faInstagram} className="icon yt"/>    
-                    </a>
-                </div>
-            </header>
+  const styles = {
+    highlight: {
+      color: '#007bff',
+      fontWeight: 'bold'
+    }
+  };
+
+  return (
+    <div className="HomePage">
+      
+      <header className="hero">
+        
+        <div className="profile">
+          <img src={avatar} alt=""/>
         </div>
-    )
+        
+        <h1 className="hero-text">
+          Hi, I'm <span> Kyle </span>
+        </h1>
+        
+        <p className="h-sub-text">
+          A software engineer focused on 
+          <span style={styles.highlight}> Full Stack Development</span>, 
+          <span style={styles.highlight}> Data Pipelines</span>, 
+          and <span style={styles.highlight}>Technical Architecture</span> 
+          in People Analytics at 
+          <a href='https://www.roche.com'>Roche</a>. 
+          <br />
+          SMB Owner of <a href='https://www.theclearicecompany.com'>The Clear Ice Company</a>
+        </p>
+        
+        <div className="icons">
+          <a href='https://www.linkedin.com/in/kylestewartfrantz/' className="icon-holder">
+            <FontAwesomeIcon icon={faLinkedin} className="icon fb"/>
+          </a>
+
+          {/* Commented out GitHub icon
+          <a href='https://www.github.com/kylesf/' className="icon-holder">
+            <FontAwesomeIcon icon={faGithub} className="icon gh"/>
+          </a>
+          */}
+
+          <a href='https://instagram.com/theclearicecompany/' className="icon-holder">
+            <FontAwesomeIcon icon={faInstagram} className="icon yt"/>
+          </a>
+        </div>
+      
+      </header>
+    
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
